f5ab7ac84e981e9518799b640e493c109e9e9a99
nickgp14 <nicoleiveth2017@gmail.com>
2025-04-24 23:32:11 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,6 +1,17 @@
 def registrarEstudiante(nombreCompleto,genero,carne,correo,notas,bdEstudiantes):
+     """
+     Funcionamiento: Crea el formato para el nuevo estudiante y lo registra en la base de datos.
+     -Entradas
+     nombreCompleto: Tupla con el nombre, primer apellido y segundo apellido del estudiante.
+     genero: True si es masculino y False si es femenino.
+     carne: Número de carné del estudiante.
+     correo: Correo institucional del estudiante.
+     notas: Tupla con las notas del estudiante
+     bdEstudiantes: Lista que tiene los demás estudiantes.
+     -Salidas
+     mensaje: Le informa al usuario si el estudiante fué registrado con éxito.
+     """
      nuevoEstudiante=[nombreCompleto,genero,carne,correo,notas]
      bdEstudiantes.append(nuevoEstudiante)
-     print (bdEstudiantes)
-     return"Estudiante registrado exitosamente"
+     return"¡Estudiante registrado exitosamente!"
     
